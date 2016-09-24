11465a46845a83cf9dfafe636992871878fcab11
Andrew Mokotoff <amokotoff@gmail.com>
2016-09-24 17:49:31 -0400

    -Added person object

--- models/person.js
+++ models/person.js
@@ -3,3 +3,44 @@
  */
 
 // Person object. This will be implemented by Student and Homeowner
+
+// person object
+
+Person = new Meteor.Collection( 'person' );
+
+PersonSchema = new SimpleSchema({
+    "first": {
+        type: String,
+        label: "First Name"
+    },
+    "last": {
+        type: String,
+        label: "Last Name"
+    },
+    "email": {
+        type: String,
+        label: "Email Address"
+    },
+    "phone": {
+        type: String,
+        label: "Phone Number"
+    },
+    "picture": {
+        type: Buffer,
+        label: "Personal Picture"
+    },
+    "location.$": {
+        type: Object
+    },
+    "location.$.lat": {
+        type: Number,
+        label: "Latitude coordinates of location"
+    },
+    "location.$.long": {
+        type: Number,
+        label: "Longitude coordinates of location"
+    }
+});
+
+Person.attachSchema( PersonSchema );
+
